6be667b6246129099efef280e54af35b5d52a170
MatheusRw <mt.richaard@hotmail.com>
2026-01-12 18:59:31 -0300

    Criando o schema

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
+from http import HTTPStatus 
+from schemas import Message,UserSchema,UserPublic
 
 app = FastAPI()
 
-@app.get('/')
+@app.get('/',status_code=HTTPStatus.OK,response_model=Message)
 
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+@app.post('/users/',status_code=HTTPStatus.CREATED,response_model=UserPublic)
+def create_user(user: UserSchema):
+    return user  
