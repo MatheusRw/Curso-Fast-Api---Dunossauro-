e2207b3b1cff79a3558684358add528db7bbb9ad
MatheusRw <mt.richaard@hotmail.com>
2026-01-16 18:38:07 -0300

    criando conftest

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,30 +1,30 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from app import app
-
-
-def test_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)
+def test_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get("/")
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {"message": "Olá Mundo!"}
 
 
-def test_create_user():
-    client = TestClient(app)
+def test_create_user(client):
+    # Com a adição do pytest fixture não é mais necessário
+    # criar o client dentro de cada teste.
+    #
+    # Basta passar o client como parâmetro que o pytest
+    # cuida de criar e injetar automaticamente.
     response = client.post(
         "/users/",
         json={
             "username": "matheus",
             "password": "123456",
-            "email": "teste@teste.com"
-        }
+            "email": "teste@teste.com",
+        },
     )
+
     assert response.status_code == HTTPStatus.CREATED
     assert response.json() == {
         "username": "matheus",
         "email": "teste@teste.com",
-        "id": 1
+        "id": 1,
     }
