6d88dc71b29add5a2c95894e1ad06f5f0de52400
MatheusRw <mt.richaard@hotmail.com>
2026-06-11 23:43:29 -0300

    criando os testes de database

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,7 +1,11 @@
-from models import User
+from sqlalchemy import create_engine
+
+from models import User, table_registry
 
 
 def teste_create_user():
+    engine = create_engine("sqlite:///database.db")
+    table_registry.metadata.create_all(engine)
     user = User(username="matheus_teste",
                 password="123456",
                 email="email@boladao.com")
